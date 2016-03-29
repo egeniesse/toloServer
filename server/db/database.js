84c6e59256cbbb55b5c698f09d365c4911bdf19e
Eric Geniesse <egeniesse@gmail.com>
2016-03-29 09:09:42 -0700

    Updates password variable

--- server/db/database.js
+++ server/db/database.js
@@ -1,6 +1,6 @@
 var request = require("request");
 
-var userPass = new Buffer(process.env.DB_USERNAME + ":" + process.env.DB_PASSWORD).toString('base64');
+var dbAuthHeader = new Buffer(process.env.DB_USERNAME + ":" + process.env.DB_PASSWORD).toString('base64');
 
 // this is the interface between the server and the database
 module.exports.cypherQuery = function(query, params, callback) {
@@ -16,7 +16,7 @@ module.exports.cypherQuery = function(query, params, callback) {
       uri: process.env.DATABASE_URL,
       json: {statements: [{statement: query, parameters: params}]},
       headers: {               
-        'Authorization': 'Basic ' + userPass                  
+        'Authorization': 'Basic ' + dbAuthHeader                  
       }, 
     },
     function (err, res, body) {
