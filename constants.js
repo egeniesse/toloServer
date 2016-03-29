bb4516c5d233e42b76cf6dfc5c0bc9d424a57059
Eric Geniesse <egeniesse@gmail.com>
2016-03-29 09:05:22 -0700

    Finishing refactoring the env vars

--- constants.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	API_URL : process.env.API_URL || "http://127.0.0.1:3000",
-	DATABASE_URL : process.env.DATABASE_URL || 'http://datingapp.sb02.stations.graphenedb.com:24789/db/data/transaction/commit'
-};
--- server/db/database.js
+++ server/db/database.js
@@ -1,8 +1,5 @@
 var request = require("request");
-var constants = require('../../constants.js');
 
-// process.env.DB_USERNAME = 'datingApp';
-// process.env.DB_PASSWORD = 'JM5vEJXuEsfNzESM2D3K';
 var userPass = new Buffer(process.env.DB_USERNAME + ":" + process.env.DB_PASSWORD).toString('base64');
 
 // this is the interface between the server and the database
@@ -16,7 +13,7 @@ module.exports.cypherQuery = function(query, params, callback) {
     params = {};
   }
   request.post({
-      uri: constants.DATABASE_URL,
+      uri: process.env.DATABASE_URL,
       json: {statements: [{statement: query, parameters: params}]},
       headers: {               
         'Authorization': 'Basic ' + userPass                  
